a2cf0f54b4d1f836f0b394d0286d642b653455ed
ShayFae <ifrahnoor108@gmail.com>
2022-01-20 19:12:35 -0500

    Update

--- breedFetcher.js
+++ breedFetcher.js
@@ -10,14 +10,15 @@ const request = require('request');
 //       output: process.stdout
 //     });
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
-  const data = JSON.parse(body);
-    // console.log(data[0].description)
-  // console.log(typeof data);
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', typeof body); // Print the HTML for the Google homepage.
-});
+
+// request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+//   const data = JSON.parse(body);
+//     // console.log(data[0].description)
+//   // console.log(typeof data);
+//   // console.log('error:', error); // Print the error if one occurred
+//   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   // console.log('body:', typeof body); // Print the HTML for the Google homepage.
+// });
 
 const findBreed = function(breed) {
   request('https://api.thecatapi.com/v1/breeds/', (error, response, body) => {
